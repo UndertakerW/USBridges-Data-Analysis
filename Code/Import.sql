bd4ef2a6707e0668b9e124c0f849f4c86ffe9d71
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-11-13 21:10:02 +0800

    Update Import.sql

--- Code/Import.sql
+++ Code/Import.sql
@@ -2,7 +2,7 @@ SET GLOBAL max_allowed_packet=1024*1024*1024;
 
 set global max_execution_time=0;
 
-load data infile 'D:/CUHK/Y3T1/ERG3010/ERG3010-Project/Resources/rawdata-usbridges-csv-2016-utf8.csv'   
+load data infile 'D:/CUHK/Y3T1/ERG3010/ERG3010-Project/Data/rawdata-usbridges-csv-2016-utf8.csv'   
 into table rawdata
 fields terminated by ','  optionally enclosed by '"' escaped by '"'   
 lines terminated by '\r\n';  
