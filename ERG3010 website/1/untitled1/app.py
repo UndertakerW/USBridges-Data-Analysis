e64bcb2b1b34627a6341d79d826cb49abaaeaeb8
MoyuST <46509809+MoyuST@users.noreply.github.com>
2020-11-27 23:01:02 +0800

    add mysql query function
    add mysql query function to a button

--- ERG3010 website/1/untitled1/app.py
+++ ERG3010 website/1/untitled1/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def get_mysql_cursor():
-    conn = pymysql.connect('localhost', user="root", passwd="1020zxc..", db="erg3010", port=3306)
+    conn = pymysql.connect('localhost', user="root", passwd="MaoYu", db="reconstruct_2", port=3306)
     cursor = conn.cursor()
     return conn, cursor
 
@@ -14,6 +14,7 @@ def get_mysql_cursor():
 def index():
     test_data = []
     return render_template('index.html', test_data=test_data)
+
 def home():
     test_data1 = []
     return render_template('home.html', test_data1=test_data1)
@@ -22,18 +23,51 @@ def home():
 @app.route('/search', methods=['POST'])
 def search_data():
     conn, cursor = get_mysql_cursor()
-    keyword = request.form.get("search")
-    country = request.form.get("country")
-    if keyword:
-        select_sql_str = f"select * from rawdata where  COUNTY_CODE_003 like '%{country}%'  limit 0,20"
-        cursor.execute(select_sql_str)
-        data = cursor.fetchall()
-        if data:
-            data = list(data)
-            test_data = [list(item) for item in data]
-        else:
-            test_data = []
-        return render_template('index.html', test_data=test_data)
+    info_list = [request.form.get("statecode"), request.form.get("countrycode"), request.form.get("year1"),
+                request.form.get("year2"), request.form.get("highway"), request.form.get("struct"), 
+                request.form.get("option_1"), request.form.get("option_2")
+    ]
+
+    query = 'select STATE_CODE_001, HIGHWAY_DISTRICT_002, COUNTY_CODE_003, STRUCTURE_NUMBER_008, YEAR_BUILT_027 from rawdata '
+
+    query_conditions = ['STATE_CODE_001 = ', 'COUNTY_CODE_003 = ', 'YEAR_BUILT_027 >= ', 'YEAR_BUILT_027 <= ', 
+                            'HIGHWAY_DISTRICT_002 ', 'STRUCTURE_NUMBER_008 ']
+
+    has_constain = False
+
+    for i in range(6):
+        if(info_list[i] and info_list[i].isspace() == False and info_list[i] != ''):
+            if(has_constain == False):
+                query += ' where '
+                has_constain = True
+            else:
+                query += ' and '
+            
+            if(i <= 3):
+                query += (query_conditions[i] + '\'' + info_list[i] + '\'')
+            else:
+                query += query_conditions[i]
+                if(info_list[i+2] == "0"):
+                    query += ('= \'' + info_list[i] + '\'')
+                elif(info_list[i+2] == "1"):
+                    query += ('like \'%' + info_list[i] + '\'')
+                elif(info_list[i+2] == "2"):
+                    query += ('like \'%' + info_list[i] + '%\'')
+                else:
+                    query += ('like \'' + info_list[i] + '%\'')
+
+    print(query)
+    print(info_list[6])
+    print(info_list[7])            
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+    if data:
+        data = list(data)
+        test_data = [list(item) for item in data]
+    else:
+        test_data = []
+    return render_template('index.html', test_data=test_data)
 
 
 
