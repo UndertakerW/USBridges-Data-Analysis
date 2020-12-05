8d405961e2eeeb7637846f99ad47f68251f30607
Sylviachelsea <43447503+Sylviachelsea@users.noreply.github.com>
2020-12-05 15:52:08 +0800

    zsq

--- Code/Step4_Question1.sql
+++ Code/Step4_Question1.sql
@@ -1,11 +1,15 @@
+drop table if exists bridge_count;
 create table bridge_count as
-select STATE_CODE_001,Structure_number_008,record_type_005A,fed_agency,year_built_027
+select STATE_CODE_001,Structure_number_008,record_type_005A,year_built_027
 from rawdata
 group by state_code_001,STRUCTURE_NUMBER_008
 having count(RECORD_TYPE_005A)=1;
 
-SELECT year_built_027 as count_year,count(year_built_027) as count
+drop table if exists bridge_count1;
+create table bridge_count1 as
+SELECT STATE_CODE_001 as count_state, year_built_027 as count_year,count(STATE_CODE_001) as count
 from bridge_count
-group by year_built_027
-order by count desc
-limit 1;
+group by year_built_027,state_code_001;
+
+select count_state,count_year,max(count) from bridge_count1
+group by count_state ;
